7fbf17fcf9f42d8d8136d2cf3d4e0d77f5c1f000
Marcos Albuquerque <mpdsa20@gmail.com>
2022-04-28 10:40:43 -0300

    Requisito 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,26 @@
+from collections import defaultdict
+
+
 def is_anagram(first_string, second_string):
-    """ Fiz o código aqui. """
+    if first_string == "" or second_string == "":
+        return False
+
+    letters_counted1 = letter_counter(first_string.lower())
+    letters_counted2 = letter_counter(second_string.lower())
+
+    return letters_counted1 == letters_counted2
+
+
+def letter_counter(word):
+    letter_counter = defaultdict(int)
+
+    for l in word:
+        letter_counter[l] += 1
+
+    return letter_counter
+
+
+# references
+# https://app.betrybe.com/course/computer-science/introducao-a-python/aprendendo-python/
+# https://docs.python.org/pt-br/3/library/collections.html#collections.defaultdict
+# https://www.geeksforgeeks.org/defaultdict-in-python/
